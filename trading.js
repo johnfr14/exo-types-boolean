a4a43870bde6980cc6b827811e06bc8e6a4623e8
johnfr14 <jonathan.alyra@gmail.com>
2021-02-15 15:47:59 +0100

    2nd commit

---
+++ trading.js
@@ -0,0 +1,10 @@
+let btc = 47500
+
+if (btc >= 100000)
+  console.log("ok c'est le moment de tp (une parie biensure pas tout !!!)")
+else if (btc < 100000 && btc >=75000)
+  console.log("Attention zone dangeureuse, on ne fait rien")
+else if (btc < 75000)
+  console.log("ALL IN GO BUY a ce prix là c'est du free BTC !!!!! à nous la lambo dans 10 ans")
+else 
+console.log("c koa le bitkoin ?")
